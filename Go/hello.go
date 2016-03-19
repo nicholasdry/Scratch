74391f69b03e905fac65efc459c26abe5374bf93
Nicholas Dry <nicholasdry@me.com>
2016-03-19 01:20:27 -0400

    Updated

---
+++ Go/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+      // fmt.Println("Hello")
+
+      // godoc fmt <Function>
+
+      // var age int = 40 if you create variables you dont use, it will complain
+
+      randNum := 1
+
+      fmt.Println(randNum, " Hello")
+
+      // constants require data type declaration
+      const pi float64 = 3.14159
+
+      // var (
+      //       varA = 2
+      //       varB = 3
+      // )
+
+      // name := "Nicholas Dry"
+      // var name string = "Nicholas Dry"
+
+      var name string = "Nicholas Dry"
+
+      fmt.Println(name)
+
+
+}
