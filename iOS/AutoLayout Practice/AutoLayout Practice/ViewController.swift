a796ca079deba23d6e8f15cc061ea4e39570dade
Nicholas Dry <nicholasdry@me.com>
2016-03-20 23:55:39 -0400

    Updated

---
+++ iOS/AutoLayout Practice/AutoLayout Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout Practice
+//
+//  Created by Nicholas Dry on 3/20/16.
+//  Copyright © 2016 Nicholas Dry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
