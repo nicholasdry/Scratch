2ce1f27068a45abd54f2aeecf82e9c4b7c49c9cb
Nicholas Dry <nicholasdry@me.com>
2016-04-04 12:37:49 -0400

    Updated

---
+++ Python/dictionaries.py
@@ -0,0 +1,6 @@
+dict = {'Name': "Nicholas", 
+	'Age': 21
+	};
+
+print "dict['Name']: ", dict['Name']
+print "dict['Age']: ", dict['Age']
---
+++ Python/math.py
@@ -0,0 +1,6 @@
+number = long(raw_input("Enter a Number > "))
+
+for i in range(1, number):
+	number *= i
+
+print number
