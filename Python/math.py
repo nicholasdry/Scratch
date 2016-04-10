4b73e81c115d2bc0237e132946c6fc7f3c762a93
Nicholas Dry <nicholasdry@me.com>
2016-04-10 12:45:07 -0400

    Updated

--- Python/math.py
+++
@@ -1,6 +0,0 @@
-number = long(raw_input("Enter a Number > "))
-
-for i in range(1, number):
-	number *= i
-
-print number
--- Python/quizGen.py
+++ Python/quizGen.py
@@ -1,3 +1,5 @@
+import random
+
 # Quiz data.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -21,4 +23,30 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 # Generates the quiz files.
 for i in range(35):
     quizFile = open('quizName%s.txt' % (i + 1), 'w')
-    answerKeyFile = open('quizAnswers')
+    answerKeyFile = open('quizAnswers%s.txt' % (i + 1), 'w')
+
+    quizFile.write((' ' * 20) + 'Quiz Name (Form %s)' % (i + 1))
+    quizFile.write('\n\n')
+
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    for questionNum in range(50):
+
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
+
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+
+    quizFile.close()
+    answerKeyFile.close()
