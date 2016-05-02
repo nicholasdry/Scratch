88f92d2e139238c4289271249deee962dea9e138
Nicholas Dry <nicholasdry@me.com>
2016-05-02 01:57:59 -0400

    Updated

---
+++ Python/passManager.py
@@ -0,0 +1,11 @@
+import sys
+import pyperclip
+
+def hashPass(password):
+	output = ""
+	for x in range(0, len(password)):
+
+# Add in a new password to the dictionary, it will be in the third index of argv
+
+if sys.argv[1] == "add":
+	print("add")	
