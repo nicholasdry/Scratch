d2a947ae15496e40d1391759833b59b9a38a947b
Nicholas Dry <nicholasdry@me.com>
2016-05-15 11:35:58 -0400

    Updated

--- C/wordperm.c
+++ C/wordperm.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct character {
+      int active;
+      int count;
+} chars;
+
+chars character[26];
+
+int factorial(int a) {
+      if (a == 0) {
+            return 1;
+      }
+      return a * factorial(a - 1);
+}
+
+int main(int argc, char **argv) {
+
+      int i;
+      while (i < 27) { // initialize array
+            character[i] = 0;
+            i++;
+      }
+
+      for (i = 0; i < strlen(argv[1]); i++) {
+
+      }
+
+}
