20d86271ac8348e25603320eafe4f17de6ff401d
Nicholas Dry <nicholasdry@me.com>
2016-05-25 18:59:53 -0400

    Updated

---
+++ Python/PythonBook/variables.py
@@ -0,0 +1,21 @@
+hello_str = "Hello World"
+
+hello_int = 21
+
+hello_bool = True
+
+hello_tuple = (21,32)
+
+hello_list = ["Hello, ", "this", "is", "a", "list"]
+
+hello_list = list()
+
+hello_list.append("Hello,")
+
+hello_dict = {
+	"firstName": "Nicholas",
+	"lastName": "Dry",
+	"eyeColor": "Hazel"
+}
+
+print(hello_dict["firstName"])
