583759028f055a018f47b797cad0be5e1cb63f56
Nicholas Dry <nicholasdry@me.com>
2016-05-25 20:01:23 -0400

    Updated

--- Python/PythonBook/lambdaFunctions.py
+++ Python/PythonBook/lambdaFunctions.py
@@ -9,6 +9,8 @@ print(two(3))
 print(three(3))
 
 # Lambda for summing a list
-
 testList = [1,2,3,4,5,6,7,8,9]
 print(reduce(lambda x,y: x+y, testList))
+
+# Lambda for finding even numbers
+print(filter(lambda x: x % 2 == 0, testList))
