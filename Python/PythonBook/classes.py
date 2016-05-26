e2a0815540302653d48f49f2429367413cc9967b
Nicholas Dry <nicholasdry@me.com>
2016-05-25 22:16:58 -0400

    Updated

---
+++ Python/PythonBook/classes.py
@@ -0,0 +1,10 @@
+class Person:
+	age = 21
+
+nicholas = Person()
+
+print(nicholas.age)
+
+nicholas.age = 22
+
+print(nicholas.age)
---
+++ Python/PythonBook/newLineSentence.py
@@ -0,0 +1,6 @@
+import sys # for sys.argv
+
+words = sys.argv[1].split()
+
+for x in words:
+	print(x)
