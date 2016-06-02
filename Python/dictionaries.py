7259093633a8f467a9f3c62267ba55d962d5c602
Nicholas Dry <nicholasdry@me.com>
2016-06-02 12:55:42 -0400

    Updated

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -1,6 +1,8 @@
 # Notes:
 
-# Dictionaries act like hash tables, especially when you have a nested list inside of 
+# Dictionaries act like hash tables, especially when you have a nested list inside of a key
+# Adding new keys is as simple as trying to access keys which don't exist
+
 
 directory = {
 		'Client': ['Nicholas Dry']
@@ -11,3 +13,12 @@ print(directory['Client'])
 directory['Client'].append('Brian Dry')
 
 print(directory['Client'])
+
+directory['Departments'] = ['Sales']
+directory['Departments'].append('Development')
+
+print(directory)
+
+directory.pop('Departments')
+
+print(directory)
