8d29b3bad3f4684a14940d93e5a3d7c1474b2f71
Nicholas Dry <nicholasdry@me.com>
2016-06-03 10:45:29 -0400

    Updated

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -1,7 +1,11 @@
 # Notes:
 
 # Dictionaries act like hash tables, especially when you have a nested list inside of a key
-# Adding new keys is as simple as trying to access keys which don't exist
+# Adding new keys is as simple as trying to access keys which don't exist dictionary['new key'] = ...
+# Searching in a dictionary is as simple as "Key" in <dictionary name>
+# Deleting values is dictionary.pop('key value')
+# To return all keys inside of a dictionary, dictionary.keys()
+# To return all values inside of a dictionary, dictionary.values()
 
 
 directory = {
