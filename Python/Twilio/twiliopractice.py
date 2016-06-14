5b285c64a9857fbf8fb6903327996cf1892e3e5c
Nicholas Dry <nicholasdry@me.com>
2016-06-14 15:29:04 -0400

    Updated

--- Python/Twilio/twiliopractice.py
+++ Python/Twilio/twiliopractice.py
@@ -1,8 +1,8 @@
 from twilio.rest import TwilioRestClient
 
 # put your own credentials here
-ACCOUNT_SID = "AC684184f26e7ac3088a17c73be11536a8"
-AUTH_TOKEN = "e0f237315288cc5ca43eb30e3d2655b5"
+ACCOUNT_SID = ""
+AUTH_TOKEN = ""
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
@@ -11,7 +11,7 @@ text = str(raw_input("What message would you like to send > "))
 
 for i in range(1, number+1):
 	client.messages.create(
-    		to="+19736990228",
-    		from_="+18566197129",
+    		to="",
+    		from_="",
     		body=text,
 	)
