f330488949197236379ba915bde8178f520f0ca1
Nicholas Dry <nicholasdry@me.com>
2016-07-13 19:57:46 -0400

    Updated

---
+++ Python/Math/Math.py
@@ -0,0 +1,11 @@
+import math
+
+# N choose K
+def nCr(n, r):
+	return math.factorial(n)/(math.factorial(n-r)*math.factorial(r))
+
+# N permute K
+def nPr(n, r):
+	return math.factorial(n)/math.factorial(n-r)
+
+
