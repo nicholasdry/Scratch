f6a3fd3e6f66de7cac33fc68e2f0ef8ba07bcfb2
Nicholas Dry <nicholasdry@me.com>
2016-10-09 11:37:32 -0400

    Haskell Notes

---
+++ Haskell/double.hs
@@ -0,0 +1,3 @@
+double x = x + x
+doubleTwo x y = (x+y) + (x+y)
+doubleSmallNumber x = (if x > 100 then x else x*2) + 1
---
+++ Haskell/lists.hs
@@ -0,0 +1,3 @@
+numbers = [1,2,3,4,5]
+numbers1 = [6,7,8,9,0]
+letters = ['h','e','l','l','o']
