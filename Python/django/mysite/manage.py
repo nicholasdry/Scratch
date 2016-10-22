7e35b780ddee2fca1200c1484af4b4232ba4e25f
Nicholas Dry <nicholasdry@me.com>
2016-10-22 18:02:55 -0400

    added GUI code

--- Python/django/mysite/manage.py
+++ Python/django/mysite/manage.py
@@ -1,10 +1,9 @@
-#Nicholas Dry
 #!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firstdjango.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
 
